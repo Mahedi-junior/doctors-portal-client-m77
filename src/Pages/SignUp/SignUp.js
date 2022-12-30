401e7739365c1ff2327351ee4b0bc1a440c5d8c1
Mahedi-junior <mahedih.cse@gmail.com>
2022-12-31 00:32:04 +0600

    login and signup page added

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -2,30 +2,43 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
   const {
     register,
-    formState: { errors },
     handleSubmit,
+    formState: { errors },
   } = useForm();
 
-  const handleLogin = (data) => {
+  const handleSignUp = (data) => {
     console.log(data);
   };
-
   return (
     <div className="h-[800px] flex justify-center items-center">
       <div className="w-96">
-        <h2 className="text-4xl text-center">Login</h2>
+        <h2 className="text-4xl text-center">SignUp</h2>
 
-        <form onSubmit={handleSubmit(handleLogin)}>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", { required: "Name is Required" })}
+              className="input input-bordered "
+            />
+            {errors.name && (
+              <p className="text-red-600">{errors.name?.message}</p>
+            )}
+          </div>
+
           <div className="form-control ">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
-              type="text"
-              {...register("email", { required: "Email Address is required" })}
+              type="email"
+              {...register("email", { required: true })}
               className="input input-bordered "
             />
             {errors.email && (
@@ -43,30 +56,32 @@ const Login = () => {
                 required: "Password is required",
                 minLength: {
                   value: 6,
-                  message: "Password must be 6 character or longer",
+                  message: "Password must be 6 characters long",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message:
+                    "Password must have uppercase, number and special characters",
                 },
               })}
-              className="input input-bordered "
+              className="input input-bordered w-full max-w-xs"
             />
             {errors.password && (
-              <p className="text-red-600">{errors.password?.message}</p>
+              <p className="text-red-500">{errors.password.message}</p>
             )}
-            <label className="label">
-              <span className="label-text">Forget Password</span>
-            </label>
           </div>
 
           {/* <p>{data}</p> */}
           <input
-            className="btn btn-accent w-full"
-            value="Login"
+            className="btn btn-accent w-full mt-5"
+            value="Sign Up"
             type="submit"
           />
         </form>
         <p>
-          New to Doctors Portal
-          <Link className="text-secondary" to="/signup">
-            Create New Account
+          Already have an account
+          <Link className="text-secondary" to="/login">
+            Please Login
           </Link>
         </p>
         <div className="divider">OR</div>
@@ -76,4 +91,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
