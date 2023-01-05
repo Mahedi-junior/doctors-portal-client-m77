946b79dbcc935cb9824f98a868208863aba53b50
Mahedi-junior <mahedih.cse@gmail.com>
2023-01-06 00:12:43 +0600

    imgbb api added

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useForm } from "react-hook-form";
+import Loading from "../../Shared/Loading/Loading";
 
 const AddDoctor = () => {
   const {
@@ -7,11 +9,24 @@ const AddDoctor = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const imageHostKey = process.env.REACT_APP_imgbb_Key;
+
+  const { data: specialties, isLoading } = useQuery({
+    queryKey: ["specialty"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/appointmentSpecialty");
+      const data = await res.json();
+      return data;
+    },
+  });
 
   const handleAddDoctor = (data) => {
     console.log(data);
   };
 
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div className="w-96">
       <h2 className="text-4xl">Add Doctors</h2>
@@ -50,17 +65,27 @@ const AddDoctor = () => {
             <span className="label-text">Specialty</span>
           </label>
 
-          <select className="select select-bordered select-ghost">
-            <option disabled selected>
-              Pick a Specialty
-            </option>
-            <option>Han Solo</option>
-            <option>Greedo</option>
+          <select
+            {...register("specialty")}
+            className="select select-bordered select-ghost"
+          >
+            {specialties.map((specialty, i) => (
+              <option key={specialty._id} value={specialty.name}>
+                {specialty.name}
+              </option>
+            ))}
           </select>
-
-          {errors.password && (
-            <p className="text-red-500">{errors.password.message}</p>
-          )}
+        </div>
+        <div className="form-control ">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input
+            type="file"
+            {...register("img", { required: "Photo is Required" })}
+            className="input input-bordered "
+          />
+          {errors.img && <p className="text-red-600">{errors.img?.message}</p>}
         </div>
 
         {/* <p>{data}</p> */}
